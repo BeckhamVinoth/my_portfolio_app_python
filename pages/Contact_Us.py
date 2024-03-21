34b2afcb8b2f059ca9302a18086f1d3bbc99eda3
BeckhamVinoth <vinoth.10decoders@gmail.com>
2024-03-21 18:14:49 +0530

    Email validation

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,8 +1,34 @@
 import streamlit as st
+import re
+from send_email import send_email_from_users
+
 
 st.header('Contact Me')
+
+
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+
 
 with st.form(key='email_form'):
     email_add = st.text_input("Enter your email address")
     email_body = st.text_area("Drop in your message")
-    st.form_submit_button("Submit")
+    message = f"""\
+Subject: New email from {email_add}
+        
+From: {email_add}
+
+{email_add} \n {email_body}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        if is_valid_email(email_add):
+            send_email_from_users(message)
+            st.success("Email sent successfully!")
+        else:
+            st.error("Invalid email address. Please enter a valid email.")
--- send_email.py
+++ send_email.py
@@ -1,23 +1,15 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-mid = 'beckhamvino93@gmail.com'
-pwd = 'teec iuij lszr dgcx'
+def send_email_from_users(message):
+    host = "smtp.gmail.com"
+    port = 465
+    mid = 'beckhamvino93@gmail.com'
+    pwd = 'teec iuij lszr dgcx'
+    receiver_mid = 'vinoth.10decoders@gmail.com'
 
-receiver_mid = 'vinoth.10decoders@gmail.com'
-
-context = ssl.create_default_context()
-
-message = """\
-Subject: Python Testing by Beckham
-Hey Pooran
-Repeat uh !!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(mid, pwd)
-    server.sendmail(mid, receiver_mid, message)
-
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(mid, pwd)
+        server.sendmail(mid, receiver_mid, message)
